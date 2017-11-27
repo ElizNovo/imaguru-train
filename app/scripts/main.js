8d5bc733be4e9a9afb9caf3afe36afee3a3bad0b
Kseniya Varabei <kseniya.varabei@gmail.com>
2017-11-27 14:35:45 +0100

    add more styles for mobile nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,20 @@
 $(window).on('scroll touchmove', function () {
+
   if ($('.menu-btn:checked').length === 0) {
 
+    let coloredSections = $('section[data-color]');
 
-  let coloredSections = $('section[data-color]');
+    const scrollTop = $(document).scrollTop();
 
-  const scrollTop = $(document).scrollTop();
-
-  coloredSections.each(function (sectionIndex, sec) {
-    let section = $(sec);
-    if (scrollTop >= section.position().top) {
-      $('body').css('background', section.attr('data-color'));
-    }
-  });
-}
+    coloredSections.each(function (sectionIndex, sec) {
+      let section = $(sec);
+      if (scrollTop >= section.position().top) {
+        $('body').css('background', section.attr('data-color'));
+      }
+    });
+  }
 });
+
+$('.menu-btn').click(function () {
+  ($('body').toggleClass('noscroll')) ;
+})
